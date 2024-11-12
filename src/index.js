e8048eda6c70c144e919cabf74e5de052269d035
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-11-12 17:31:49 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App></App>,
+  },
+  {
+    path: "",
+    element: <></>,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
