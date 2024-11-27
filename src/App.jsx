9e74247c9f4acd3e5d8a9108b198e7cbda79162a
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-11-27 12:54:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav';
 
 function App() {
   return (
-    <div>
+    <div class="pro">
       <Nav></Nav>
       <Outlet></Outlet>
     </div>
--- src/features/productitems/AddProduct.jsx
+++ src/features/productitems/AddProduct.jsx
@@ -7,7 +7,7 @@ function AddProduct() {
     var navigate=useNavigate();
     var[AddFn]=useAddproductsMutation();
   return (
-    <div class="container">
+    <div class="container rounded p-2 addpro">
       <Formik initialValues={{
         id:"",
         title:"",
@@ -30,36 +30,36 @@ function AddProduct() {
       }}
       >
         <Form>
-        <div class="form-floating mb-3">
-          <Field type="text" name="id" class="form-control" id="id" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="id" class="form-control f" id="id" placeholder="name@example.com"></Field>
           <label for="id">id</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="title" class="form-control" id="title" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="title" class="form-control f" id="title" placeholder="name@example.com"></Field>
           <label for="title">Title</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="price" class="form-control" id="price" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="price" class="form-control f" id="price" placeholder="name@example.com"></Field>
           <label for="price">Price</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="description"  class="form-control" id="description" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="description"  class="form-control f" id="description" placeholder="name@example.com"></Field>
           <label for="description">Description</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="category" class="form-control" id="category" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="category" class="form-control f" id="category" placeholder="name@example.com"></Field>
           <label for="category">Category</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="image" class="form-control" id="image" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="image" class="form-control f" id="image" placeholder="name@example.com"></Field>
           <label for="image">Image</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="rate" class="form-control" id="rate" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="rate" class="form-control f" id="rate" placeholder="name@example.com"></Field>
           <label for="rate">Rate</label>
         </div>
-        <div class="form-floating mb-3">
-          <Field type="text" name="count" class="form-control" id="count" placeholder="name@example.com"></Field>
+        <div class="form-floating mb-3 p-2 m-2">
+          <Field type="text" name="count" class="form-control f" id="count" placeholder="name@example.com"></Field>
           <label for="count">Count</label>
         </div>
         <button type='submit' class="btn btn-outline-primary">ADD Product</button>
