03e640c4af94a8eaba97fa65175183affb35b5d7
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-12-02 19:25:21 +0530

    changes

--- src/features/User/Login.jsx
+++ src/features/User/Login.jsx
@@ -1,32 +1,19 @@
 import React from 'react'
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Field, Form } from 'formik';
 
 function Login() {
   return (
     <div>
       <Formik
        initialValues={{ username: '', password: ''}}
-       validationSchema={Yup.object({
-         username: Yup.string()
-           .max(12, 'Must be 12 characters or less')
-           .required('Required'),
-         lastName: Yup.string()
-           .max(20, 'Must be 20 characters or less')
-           .required('Required')
-       })}
        onSubmit={(values) => {
          console.log(values)
        }}
      >
        <Form>
-         <label htmlFor="username">Username</label>
          <Field name="username" type="text" />
-         <ErrorMessage name="username" />
          <br />
-         <label htmlFor="password">Password</label>
          <Field name="password" type="text" />
-         <ErrorMessage name="password" />
          <br />
          <button type="submit">Login</button>
        </Form>
