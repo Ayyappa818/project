2426e000866da22c4ae1c8d945bf6c5ddeae2b30
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-12-03 12:16:48 +0530

    changes

--- src/services/UserApi.jsx
+++ src/services/UserApi.jsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const UserApi = createApi({
   reducerPath: 'UserApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:6000/' }),
   endpoints: (builder) => ({
 
     signup: builder.mutation({
